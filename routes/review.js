33d1824ddd7969b2796f518d0479001b949ec82b
Zuhair Ahmad <zuhair2945@gmail.com>
2025-06-13 13:04:32 +0530

    fixed reviews bug

--- routes/review.js
+++ routes/review.js
@@ -4,6 +4,7 @@ const ExpressError = require("../utils/ExpressError.js");
 const { reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js"); 
 const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
 
 const validateReview = (req,res,next)=>{ 
     let {error} = reviewSchema.validate(req.body);
