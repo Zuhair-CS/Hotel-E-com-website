5300912bc885c337cef247a8b5b88e47d8e50dcb
Zuhair Ahmad <zuhair2945@gmail.com>
2025-06-23 02:25:08 +0530

    implmented starability library and router.route to make code compact in routes.

--- routes/User.js
+++ routes/User.js
@@ -6,14 +6,14 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/user.js");
 
-router.get("/signup", userController.renderSignupForm);
-router.post("/signup", wrapAsync(userController.signup));
+router.route("/signup")
+.get(userController.renderSignupForm) //signup route
+.post(wrapAsync(userController.signup));
 
-router.get("/login",userController.renderLoginForm );
-
-router.post("/login",saveRedirectUrl,
+router.route("/login")
+.get(userController.renderLoginForm )  //login route
+.post(saveRedirectUrl,
     passport.authenticate("local", {failureRedirect: "/login", failureFlash: true,}),userController.login );
-
+//logout route
 router.get("/logout", userController.logout);
-
 module.exports = router;
--- routes/listing.js
+++ routes/listing.js
@@ -4,18 +4,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
-//show all listings
-router.get("/",wrapAsync(listingController.index));
+router.route("/")
+.get(wrapAsync(listingController.index)) //show all listings
+.post( validateListing, wrapAsync(listingController.createListing)); //create listing
+
 //new listing
 router.get("/new",isLoggedIn, listingController.renderNewForm);
-//show listing
-router.get("/:id", wrapAsync(listingController.showListing));
-//create listing
-router.post("/", validateListing, wrapAsync(listingController.createListing));
+
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))//show listing
+.put(isLoggedIn,isOwner, validateListing, wrapAsync(listingController.updateListing)) //edit listing
+.delete(isLoggedIn,isOwner, wrapAsync(listingController.destroyListing)); //delete Route
+
 //show route
 router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync( listingController.renderEditForm));
-//edit listing
-router.put("/:id",isLoggedIn,isOwner, validateListing, wrapAsync(listingController.updateListing));
-//delete Route
-router.delete("/:id",isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+
 module.exports = router;
